8cbf1b5e02be5d8d71696d52222aeeca363fc2b9
Miguel García <mggarcia@hi-iberia.es>
2019-02-21 17:18:08 +0100

    [ADD]

--- src/main/java/maven/poryect/App.java
+++ src/main/java/maven/poryect/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Heello World!" );
+        Operaciones operacion= new Operaciones();
+        int resultado= operacion.suma(2, 2);
+        System.out.println(resultado );
+        
     }
 }
---
+++ src/main/java/maven/poryect/Operaciones.java
@@ -0,0 +1,9 @@
+package maven.poryect;
+
+public class Operaciones {
+
+	public int suma(int a, int b){
+		return a+b;
+	}
+
+}
