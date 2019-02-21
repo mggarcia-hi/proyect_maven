a2fbd4200fd99553ae252cd3ea18d197ff8a8f36
Miguel García <mggarcia@hi-iberia.es>
2019-02-21 17:38:27 +0100

    [ADD]

--- src/test/java/maven/proyect/OperacionesTest.java
+++ src/test/java/maven/proyect/OperacionesTest.java
@@ -5,7 +5,15 @@ import junit.framework.TestCase;
 public class OperacionesTest extends TestCase {
 
 	public void testSuma() {
-		assertEquals(2, 2);
+		Operaciones op= new Operaciones();
+		int result= op.suma(3, 6);
+		assertEquals(result, 9);
+	}
+	
+	public void testSuma2() {
+		Operaciones op= new Operaciones();
+		int result= op.suma(3, 6);
+		assertEquals(result, 9);
 	}
 
 }
