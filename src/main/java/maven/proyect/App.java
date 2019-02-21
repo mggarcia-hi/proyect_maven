7e391d2af3cf0adef4beb90b1ac57f873ef714f0
Miguel García <mggarcia@hi-iberia.es>
2019-02-21 17:24:11 +0100

    [ADD]

--- src/main/java/maven/proyect/App.java
+++ src/main/java/maven/proyect/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Heello World!" );
+        System.out.println( "Hello World!" );
         Operaciones operacion= new Operaciones();
         int resultado= operacion.suma(2, 2);
         System.out.println(resultado);
